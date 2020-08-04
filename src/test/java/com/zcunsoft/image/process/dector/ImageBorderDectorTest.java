e1f01eccfa74242b891fb320f9c40f54f8d67280
Franklinfang <frankdevhub@gmail.com>
2020-08-04 10:45:51 +0800

    标题过滤与标的物地址的信息过滤

--- src/test/java/com/zcunsoft/image/process/dector/ImageBorderDectorTest.java
+++ src/test/java/com/zcunsoft/image/process/dector/ImageBorderDectorTest.java
@@ -31,7 +31,9 @@ public class ImageBorderDectorTest {
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
+
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	private final String SAMPLE_PATH_PREFIX = "D:/frankdevhub-workspace/samples/";
 
 	@Test
 	public void imageBorderDector() {
@@ -40,7 +42,19 @@ public class ImageBorderDectorTest {
 		// Mat src =
 		// Imgcodecs.imread("src/main/resources/samples/border_test01.jpg");
 
-		Mat src = Imgcodecs.imread("src/main/resources/cameras/camera_env02.jpg");
+		// Mat src =
+		// Imgcodecs.imread("src/main/resources/cameras/camera_env02.jpg");
+
+		/**
+		 * IMREAD_UNCHANGED=-1不进行转化,比如保存为了16位的图片，读取出来仍然为16位
+		 * IMREAD_GRAYSCALE=0进行转化为灰度图,比如保存为了16位的图片,读取出来为8位,类型为CV_8UC1
+		 * IMREAD_COLOR=1进行转化为三通道图像。
+		 * IMREAD_ANYDEPTH=2如果图像深度为16位则读出为16位,32位则读出为32位,其余的转化为8位
+		 * IMREAD_ANYCOLOR=4图像以任何可能的颜色格式读取 IMREAD_LOAD_GDAL=8使用GDAL驱动读取文件
+		 *
+		 */
+
+		Mat src = Imgcodecs.imread(SAMPLE_PATH_PREFIX + "yitulu_01.png");
 		Imgproc.resize(src, src, new Size(src.cols() / 2, src.rows() / 2));
 
 		LOGGER.debug("display source image sample");
@@ -51,17 +65,21 @@ public class ImageBorderDectorTest {
 		LOGGER.debug("invoke cvtColor{...}");
 		Mat gary = new Mat();
 		Imgproc.cvtColor(src, gary, Imgproc.COLOR_BGR2GRAY);
+
 		// 图像边缘处理
 		LOGGER.debug("invoke Canny{...}");
 		Mat edges = new Mat();
 		Imgproc.Canny(gary, edges, 200, 500, 3, false);
+
 		// 发现轮廓
 		LOGGER.debug("seach and mark image border entities");
 		List<MatOfPoint> list = new ArrayList<MatOfPoint>();
 		Mat hierarchy = new Mat();
+
 		// 查找图片对象中的轮廓
 		LOGGER.debug("invoke findContours{...}");
 		Imgproc.findContours(edges, list, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
 		// 绘制轮廓轮廓或填充轮廓
 		LOGGER.debug("invoke drawContours{...}");
 		Imgproc.drawContours(src, list, -1, new Scalar(0, 255, 0), Imgproc.LINE_4, Imgproc.LINE_AA);
